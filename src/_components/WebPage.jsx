a3b573d441d6d3b0a9a7bdf68e159ec08bc5fdc1
dhruvathakur <128789248+DhruvaPratapSingh@users.noreply.github.com>
2024-09-04 00:03:38 +0530

    fix style

--- src/_components/WebPage.jsx
+++ src/_components/WebPage.jsx
@@ -22,22 +22,22 @@ const WebPage = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex justify-between w-full mt-10">
-        <div className="w-1/2 font-extrabold m-10 p-10 text-4xl flex flex-col items-start justify-center">
+      <div className="flex flex-col justify-center items-center md:flex md:justify-between w-full mt-10 ">
+        <div className="w-1/2 font-extrabold md:font-extrabold m-10 p-10 text-2xl md:text-4xl flex flex-col items-center md:items-start">
         <div className="flex w-full m-4">
-        <h1 className="flex items-center gap-3">
+        <h1 className=" md:flex-row gap-3 text-wrap">
           <img
             src="https://dhruv-codes.vercel.app/_next/static/media/waving-hand.1da6fc7a.gif"
             alt="Waving Hand"
             className="w-14 h-14"
           />
-          hey <span className="ml-2 text-green-600">gamechangers🫵</span>
+          hey <span className="md:ml-2 text-green-600">gamechangers<span className='hidden md:block'>🫵</span></span>
         </h1>
       </div>
         <h2>Here You Can <span className="typing text-red-700"></span></h2>
         </div>
         <div className="w-1/2 m-5">
-          <img className='w-full h-full bg-transparent' src="/giify.gif" alt="Gif animation" />
+          <img className='w-full h-full flex items-center justify-center ' src="/giify.gif" alt="Gif animation" />
         </div>
       </div>
     </div>
